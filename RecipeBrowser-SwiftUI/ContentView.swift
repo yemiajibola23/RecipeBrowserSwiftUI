a44d4a43f29396d61b71428efa3ed2e1d086c6e2
Yemi Ajibola <yajibola23@gmail.com>
2023-06-30 15:45:24 -0500

    Initial Commit

---
+++ RecipeBrowser-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RecipeBrowser-SwiftUI
+//
+//  Created by Yemi Ajibola on 6/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeBrowser-SwiftUI/RecipeBrowser_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowser_SwiftUIApp.swift
+//  RecipeBrowser-SwiftUI
+//
+//  Created by Yemi Ajibola on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBrowser_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
